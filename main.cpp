be7b4a63900d3fffec7a4fdacb25b1197757acce
engineerjp <jacobpaquette33@gmail.com>
2020-08-25 23:35:19 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a;
+  int b;
+  int sum;
+
+  cout << "Enter a number!: \n";
+  cin >> a;
+
+  cout << "Enter a second number!: \n";
+  cin >> b;
+
+  sum = a + b;
+  cout << "the sum of those numbers equals " << sum << endl;
+
+  return 0;
+
+}
